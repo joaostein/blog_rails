72099b4eb2831b5702ec4dbde98caf80a2eda88d
Joao Stein <joaosteing@gmail.com>
2013-01-11 16:18:56 -0200

    First Commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+gem 'pg'
+gem 'slim'
+gem 'thin'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'compass-rails'
+  gem 'susy'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogJoao::Application.config.secret_token = 'bee61c4b5a4a058ea71ac81bab5d24de92a06d137d8b864b9cbf52fa03680b151a43da4ac625f3c3a07477fd38bfd2d41a56cba239f0d6f99a38f19c83eec119'
